6ec96dd14e4e51c56aabbcfd32525bb67af0fa35
andrew <andrewmuhn@gmail.com>
2023-03-23 14:52:56 -0700

    starting generating readme on generateMarkdown.js

---
+++ ExampleREADME.md
@@ -0,0 +1,45 @@
+# <readme-Generator>
+
+## Description
+
+abc
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+npm i
+
+## Usage
+
+
+
+## Credits
+
+[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)
+
+## License
+
+
+
+## Badges
+
+
+
+## Features
+
+
+
+## How to Contribute
+
+
+
+## Tests
+
+> npm test
+
--- index.js
+++ index.js
@@ -6,89 +6,79 @@ const generateMarkdown = require('./utils/generateMarkdown.js')
 
 // TODO: Create an array of questions for user input
 const questions = [
-  'What is the title of your project?',
-  'Describe your project?',
-  'How do you use your project?',
-  'How do you contribute to the project?',
-  'What command should be run to install dependancies', //npm i
-  'What command command should be run to run tests?', //npm test
-  'Which license do you want to use?',
-  'Please enter your GitHub username?',
-  'Please enter your email.'
+  {
+    type: 'input',
+    message: 'What is the title of your project?',
+    name: 'title'
+  },
+  {
+    type: 'input',
+    message: 'Describe your project?',
+    name: 'description'
+  },
+  {
+    type: 'input',
+    message: 'How do you use your project?',
+    name: 'usage'
+  },
+  {
+    type: 'input',
+    message: 'How do you contribute to the project?',
+    name: 'contribution'
+  },
+  {
+    type: 'input',
+    message: 'What command should be run to install dependancies',
+    name: 'installation',
+    default: 'npm i'
+  },
+  {
+    type: 'input',
+    message: 'What command command should be run to run tests?',
+    name: 'test',
+    default: 'npm test'
+  },
+  {
+    type: 'list',
+    message: 'Which license do you want to use?',
+    name: 'license',
+    choices: [
+      'MIT License',
+      'GNU GPLv3',
+      'GNU AGPLv3',
+      'GNU LGPLv3',
+      'Mozilla Public License 2.0',
+      'Apache License 2.0',
+      'Boost Siftware License 1.0',
+      'The Unlicense'
+    ],
+    loop: false
+  },
+  {
+    type: 'input',
+    message: 'Please enter your GitHub username?',
+    name: 'github'
+  },
+  {
+    type: 'input',
+    message: 'Please enter your email.',
+    name: 'email'
+  }
 ];
 
 // TODO: Create a function to write README file
 const writeToFile = (fileName, data) => {
   fsPromises.writeFile(fileName, generateMarkdown(data));
 
+
 }
 
 const promptUser = async () => {
   await inquirer
-    .prompt([
-      {
-        type: 'input',
-        message: questions[0],
-        name: 'title'
-      },
-      {
-        type: 'input',
-        message: questions[1],
-        name: 'description'
-      },
-      {
-        type: 'input',
-        message: questions[2],
-        name: 'usage'
-      },
-      {
-        type: 'input',
-        message: questions[3],
-        name: 'contribution'
-      },
-      {
-        type: 'input',
-        message: questions[4],
-        name: 'installation',
-        default: 'npm i'
-      },
-      {
-        type: 'input',
-        message: questions[5],
-        name: 'test',
-        default: 'npm test'
-      },
-      {
-        type: 'list',
-        message: questions[6],
-        name: 'license',
-        choices: [
-          'MIT License',
-          'GNU GPLv3',
-          'GNU AGPLv3',
-          'GNU LGPLv3',
-          'Mozilla Public License 2.0',
-          'Apache License 2.0',
-          'Boost Siftware License 1.0',
-          'The Unlicense'
-        ],
-        loop: false
-      },
-      {
-        type: 'input',
-        message: questions[7],
-        name: 'github'
-      },
-      {
-        type: 'input',
-        message: questions[8],
-        name: 'email'
-      },
-    ])
+    .prompt(questions)
     .then((responses) => {
       console.log(responses);
-      const fileName = `${responses.title.toLowerCase().split(' ').join('')}.md`;
-      // fs.writeFile(fileName, JSON.stringify(responses, null, '\t'), (err) => err ? console.error(err) : console.log('WooHoo!'));
+      const fileName = 'ExampleREADME.md';
       writeToFile(fileName, responses)
     })
     .then(() => console.log('success!'))
@@ -97,8 +87,13 @@ const promptUser = async () => {
 }
 
 // TODO: Create a function to initialize app
-const init = () => {
-  promptUser()
+const init = async () => {
+  try {
+    promptUser()
+
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 // Function call to initialize app
